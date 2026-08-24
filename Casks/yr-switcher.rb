aed089c7670e5e480e4a5fb827dbd6283e002753
Ruben Yakunchev <86118746+gluck-59@users.noreply.github.com>
2026-08-24 20:09:44 +0000

    Update appcast and cask for 2.1 [skip ci]

--- Casks/yr-switcher.rb
+++ Casks/yr-switcher.rb
@@ -1,8 +1,8 @@
 cask "yr-switcher" do
-  version "2.0"
-  sha256 "6edd35ef6d5d3cd8d725d7e4e275fb9b24b5639b7c8a3e46991d1b09876ab4e1"
+  version "2.1"
+  sha256 "4cff37a95d3fdb20ce00e43ff0d17071869e886e2e741f360926a57f0d8a3927"
 
-  url "https://github.com/gluck-59/yr-switcher/releases/download/v2.0/YRSwitcher.zip"
+  url "https://github.com/gluck-59/yr-switcher/releases/download/v2.1/YRSwitcher.zip"
   name "ЯR Switcher"
   desc "Convert selected text between keyboard layouts with a hotkey"
   homepage "https://github.com/gluck-59/yr-switcher"
